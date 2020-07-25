9c49ec991b423a51357127eaca2c6fa99743d12c
Hryhorii Bahalika <63339867+grigorijbagalika@users.noreply.github.com>
2020-07-25 19:53:39 +0300

    123

--- test.js
+++ test.js
@@ -1,21 +1,3 @@
-let car = {
-  maxSpeed: 280,
-  update: speedChange,
-};
+nts.includes("b");
 
-car.update();
-
-function speedChange() {
-  console.log(car.maxSpeed);
-}
-
-const age = 10;
-const totalPrice = 200.74;
-const userName = "Huck";
-const messege = "Hello world";
-const isOpen = true;
-const sholdConfirm = false;
-
-const type = typeof age;
-
-console.log(type);
+console.log(nts);
