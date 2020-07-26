402a92c1a7cb0ec326e169e3f9a7a87dc72d5cac
Hryhorii Bahalika <63339867+grigorijbagalika@users.noreply.github.com>
2020-07-26 19:42:11 +0300

    123

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-const min = 21;
-const max = 99;
-let total = 0;
-
-for (let i = min; i <= max; i += 1) {
-  console.log(i);
-
-  if (i % 2 === 0) {
-    console.log(`четное: `, i);
-  }
-}
+console.log(!false);
