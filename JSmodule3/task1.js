679d61b8eaba34573b85cf591db491010bed629c
Hryhorii Bahalika <63339867+grigorijbagalika@users.noreply.github.com>
2020-08-16 19:05:01 +0300

    5555

--- JSmodule3/task1.js
+++ JSmodule3/task1.js
@@ -8,10 +8,20 @@ const user = {
 
 //Write code under this line
 
+user.mood = "happy";
+user["full time"] = true;
+user["hobby"] = "skydiving";
+user["premium"] = false;
+
 const keys = Object.keys(user);
+
+for (const key of keys) {
+  message += `${key}: ${user[key]}\n`;
+}
+
 // Write code under this line
 
-//console.log(message);
+console.log(message);
 /*
 'age : 20
 hobby : skydiving
